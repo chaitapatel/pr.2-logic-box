6a7b1914494bd32673742a71217d7e2777c05be9
chaitapatel <chaitapatel2789@gmail.com>
2026-08-27 11:54:41 +0530

    Add files via upload

---
+++ project_2output.py
@@ -0,0 +1,42 @@
+#Welcome to the Pattern Generator and Number Analyzer!
+
+#Select an option:
+#1. Generate a Pattern
+#2. Analyze a Range of Numbers
+#3. Exit
+#Enter your choice: 1
+#Enter the number of rows for the pattern: 5
+
+#Pattern:
+#*
+#**
+#***
+#****
+#*****
+
+#Welcome to the Pattern Generator and Number Analyzer!
+
+#Select an option:
+#1. Generate a Pattern
+#2. Analyze a Range of Numbers
+#3. Exit
+#Enter your choice: 2
+#Enter the start of the range: 10
+#Enter the end of the range: 15
+#Number 10 is Even
+#Number 11 is Odd
+#Number 12 is Even
+#Number 13 is Odd
+#Number 14 is Even
+#Number 15 is Odd
+
+#Sum of all numbers from 10 to 15 is: 75
+
+#Welcome to the Pattern Generator and Number Analyzer!
+
+#Select an option:
+#1. Generate a Pattern
+#2. Analyze a Range of Numbers
+#3. Exit
+#Enter your choice: 3
+#Exiting the program. Goodbye!
